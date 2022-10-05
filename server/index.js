50832f40e66b16709e8940daee271a5f1849f82d
Nick Hays <nicholasahays@gmail.com>
2022-10-04 20:22:02 -0700

    Destructures helper object method

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
-const pg = require('pg')
+const { getQuestions } = require('./helpers.js')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.get('/qa/:product_id', (req, res) => {
+  let product_id = req.params.product_id
+  getQuestions(product_id)
+    .then(data => {
+      console.log(data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 app.listen(3000, () => {
   console.log('Server listening on port: 3000')
