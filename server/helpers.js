57cf41766a65b53c2fce9eb8f846567e476b6558
Nick Hays <nicholasahays@gmail.com>
2022-10-04 20:26:06 -0700

    Adds dotenv, imports apikey, and adds authorization header to get req

--- server/helpers.js
+++ server/helpers.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const pg = require('pg')
 const axios = require('axios')
 const BASEURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp'
+const APIKEY = process.env.APIKEY
 
 const getQuestions = product_id => {
-  return axios.get(`${BASEURL}/qa/questions?product_id=${product_id}&count=50`)
+  return axios.get(`${BASEURL}/qa/questions?product_id=${product_id}&count=50`, headers: {'Authorization': APIKEY})
 }
 
 
