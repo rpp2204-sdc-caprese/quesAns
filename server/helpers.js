151a9d7eead86a673f201f41961a78bf3a5b1ca0
Nick Hays <nicholasahays@gmail.com>
2022-10-04 20:29:20 -0700

    Adds object braces around headers object

--- server/helpers.js
+++ server/helpers.js
@@ -5,7 +5,7 @@ const BASEURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp'
 const APIKEY = process.env.APIKEY
 
 const getQuestions = product_id => {
-  return axios.get(`${BASEURL}/qa/questions?product_id=${product_id}&count=50`, headers: {'Authorization': APIKEY})
+  return axios.get(`${BASEURL}/qa/questions?product_id=${product_id}&count=50`, {headers: {'Authorization': APIKEY}})
 }
 
 
