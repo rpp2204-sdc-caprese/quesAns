a9b66935584dafd7b74cd65be860a461016bf143
Nick Hays <nicholasahays@gmail.com>
2022-10-05 17:04:40 -0700

    Imports db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const { getQuestions } = require('./helpers.js')
+const { db } = require('./etl.js')
 
 const app = express()
 
@@ -10,11 +11,13 @@ app.use(express.urlencoded({extended: true}))
 app.get('/qa/:product_id', (req, res) => {
   let product_id = req.params.product_id
   getQuestions(product_id)
-    .then(data => {
-      console.log(data.results)
+    .then(results => {
+      console.log(results.data)
+      res.send(results.data)
     })
     .catch(err => {
       console.log(err)
+      res.send(err)
     })
 })
 
