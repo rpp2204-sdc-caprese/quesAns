9f0e600f576fbf5644392f78d047785db08b4024
Nick Hays <nicholasahays@gmail.com>
2022-10-25 16:32:31 -0700

    Defaults count to 5 and page to 1

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({extended: true}))
 app.get('/qa/questions', (req, res) => {
   let product_id = req.query.product_id
   let count = req.query.count || 5
-  let page = req.query.page - 1 || 1
+  let page = req.query.page || 1
+
   getQuestions(product_id, count, page)
     .then(results => {
       console.log(results)
@@ -37,7 +38,8 @@ app.get('/qa/questions', (req, res) => {
 app.get('/qa/questions/:question_id/answers', (req, res) => {
   let question_id = req.params.question_id
   let count = req.query.count || 5
-  let page = req.query.page - 1 || 1
+  let page = req.query.page || 1
+
   getAnswers(question_id, count, page)
     .then(results => {
       res.send(results)
