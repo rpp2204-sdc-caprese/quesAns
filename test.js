d82bd9b81218cb4cbdd2e011ced4d74cb0024a18
Nick Hays <nicholasahays@gmail.com>
2022-10-30 18:29:20 -0700

    Adds status code tests for 200 questions and 404 invalid product id

--- test.js
+++ test.js
@@ -1,9 +1,35 @@
-const sum = (a, b) => {
-  return a + b
-}
+const db = require('./server/etl.js')
+const app = require('./server/index.js')
+const request = require('supertest')
 
-describe('JestTest', () => {
-  test('adds 2 + 4 to equal 6', () => {
-    expect(sum(2, 4)).toBe(6)
+// beforeAll(done => {
+//   server = app.listen(3002)
+// })
+let server;
+
+beforeEach(() => {
+  server = app.listen(4000)
+})
+
+afterEach(() => {
+  server.close()
+})
+
+describe('GET questions', () => {
+  it('Should return status code 200', async () => {
+    let product_id = 900011
+    const response = await request(server).get(`/qa/questions?product_id=900011`)///questions?product_id`)//=${product_id}`)
+    //console.log('RESPONSE', response)
+    expect(response.statusCode).toBe(200)
+  })
+
+  it('Should return status code 404 if there is no product_id',  async () => {
+    const response = await request(server).get('qa/questions')
+    expect(response.statusCode).toBe(404)
   })
 })
+
+// afterAll(done => {
+//   server.close()
+//   done()
+// })
