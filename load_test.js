8bb856b44f303e6dd5782b91aa0550dcfce569bf
Nick Hays <nicholasahays@gmail.com>
2022-11-01 18:39:54 -0700

    Uses 5000 VU load test

--- load_test.js
+++ load_test.js
@@ -8,24 +8,51 @@ import { sleep } from 'k6'
 //       rate: 500,
 //       timeUnit: '1s',
 //       duration: '1m',
-//       preAllocatedVUs: 15000,
+//       preAllocatedVUs: 5000,
 //       //maxVUs: 10000
 //     }
 //   }
 // }
 
-
 export let options = {
   insecureSkipTLSVerify: true,
   noConnectionReuse: false,
   stages: [
-    { duration: '1m', target: 1 },
-    { duration: '2m', target: 10 },
-    { duration: '2m', target: 100 },
     { duration: '2m', target: 1000 },
-    { duration: '1m', target: 0 }
+    { duration: '2m', target: 1000 },
+    { duration: '2m', target: 2000 },
+    { duration: '2m', target: 2000 },
+    { duration: '2m', target: 3000 },
+    { duration: '2m', target: 3000 },
+    { duration: '2m', target: 4000 },
+    { duration: '2m', target: 4000 },
+    { duration: '2m', target: 5000 },
+    { duration: '2m', target: 5000 },
+    { duration: '3m', target: 0 }
   ]
 }
+
+// export const options = {
+//   discardResponseBodies: true,
+
+//   scenarios: {
+//     contacts: {
+//       executor: 'ramping-arrival-rate',
+
+//       startRate: 100,
+//       timeUnit: '1s',
+//       preAllocatedVUs: 100,
+//       maxVUs: 5000,
+
+//       stages: [
+//         { target: 300, duration: '2m' },
+//         { target: 400, duration: '2m' },
+//         { target: 500, duration: '2m' },
+//         { target: 600, duration: '2m' },
+//       ]
+//     }
+//   }
+// }
 
 export default function() {
   http.get('http://localhost:3000/questions/900012')
