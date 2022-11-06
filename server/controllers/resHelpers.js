b3769bb2b05af5df20ae57a3bd8a87c8e4229f4e
Nick Hays <nicholasahays@gmail.com>
2022-11-06 16:47:35 -0700

    Adds console log err to handleError function

--- server/controllers/resHelpers.js
+++ server/controllers/resHelpers.js
@@ -2,7 +2,10 @@ const handleGetResponse = (res, data) => res.status(200).send(data)
 const handlePostResponse = (res, data) => res.status(201).send(data)
 const handlePutResponse = (res, data) => res.status(204).send(data)
 const handleClientError = (res, err) => res.status(404).send(err)
-const handleError = (res, err) => res.status(500).send(err)
+const handleError = (res, err) => {
+  console.log(err)
+  res.status(500).send(err)
+}
 
 
 module.exports = {
