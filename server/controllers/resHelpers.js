32385f238a8013dd34498b6650ff0f6c0a914a00
Nick Hays <nicholasahays@gmail.com>
2022-11-06 13:30:54 -0700

    Separates response helper functions into own file

---
+++ server/controllers/resHelpers.js
@@ -0,0 +1,14 @@
+const handleGetResponse = (res, data) => res.status(200).send(data)
+const handlePostResponse = (res, data) => res.status(201).send(data)
+const handlePutResponse = (res, data) => res.status(204).send(data)
+const handleClientError = (res, err) => res.status(404).send(err)
+const handleError = (res, err) => res.status(500).send(err)
+
+
+module.exports = {
+  handleGetResponse,
+  handlePostResponse,
+  handlePutResponse,
+  handleClientError,
+  handleError
+}
