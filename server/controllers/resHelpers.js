5798c9356c7f47af83997e04a6feb59e79fd8e40
Nick Hays <nicholasahays@gmail.com>
2022-11-11 14:55:52 -0700

    Removes data parameter from handlePut

--- server/controllers/resHelpers.js
+++ server/controllers/resHelpers.js
@@ -1,6 +1,6 @@
 const handleGetResponse = (res, data) => res.status(200).send(data)
 const handlePostResponse = (res, data) => res.status(201).send(data)
-const handlePutResponse = (res, data) => res.status(204).send(data)
+const handlePutResponse = (res) => res.sendStatus(204)
 const handleClientError = (res, err) => res.status(404).send(err)
 const handleError = (res, err) => {
   console.log(err)
