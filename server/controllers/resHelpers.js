ddcc9cb9ee0abcd2cf40e583d3359501ded1c7d3
Nick Hays <nicholasahays@gmail.com>
2022-11-13 14:03:03 -0700

    Uses sendStatus for handlePost

--- server/controllers/resHelpers.js
+++ server/controllers/resHelpers.js
@@ -1,5 +1,5 @@
 const handleGetResponse = (res, data) => res.status(200).send(data)
-const handlePostResponse = (res, data) => res.status(201).send(data)
+const handlePostResponse = (res) => res.sendStatus(201)
 const handlePutResponse = (res) => res.sendStatus(204)
 const handleClientError = (res, err) => res.status(404).send(err)
 const handleError = (res, err) => {
