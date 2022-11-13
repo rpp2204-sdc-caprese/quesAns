73e71047f631b6a191ab3a475d2bfbd645f36e30
Nick Hays <nicholasahays@gmail.com>
2022-11-13 14:02:09 -0700

    Refactors to use client instead of pool for post transaction, and fixes typos for questionIdIsValid

--- server/controllers/answerControllers.js
+++ server/controllers/answerControllers.js
@@ -8,43 +8,41 @@ const {
 
 const {
   SELECT_ANSWERS,
-  INSERT_ANSWERS,
+  INSERT_ANSWER,
   INSERT_PHOTOS,
   UPDATE_ANSWERS_HELPFULNESS,
   UPDATE_ANSWERS_REPORTED
 } = require('../../database/queries/queriesAnswers.js')
 
 const { getCache, setCache } = require('../../database/redisHelpers.js')
-
 const pool = require('../../database/db.js')
 
-const getAnswers = async (req, res) => {
 
+const getAnswers = async (req, res) => {
   let question_id = req.params.question_id
   let count = req.query.count || 5
   let page = req.query.page || 1
 
-  const productIdIsInvalid = product_id === undefined || parseInt(product_id) < 0 || product_id.length === 0
+  const questionIdIsInvalid = question_id === undefined || parseInt(question_id) < 0 || question_id.length === 0
 
-  if(productIdIsInvalid) {
+  if(questionIdIsInvalid) {
     return handleClientError(res, 'MUST HAVE VALID PRODUCT ID')
   }
 
   try {
-    let redisAnswerKey = `question_id=${question_id}&count=${count}&page=${page}`;
-    const cache = await getCache(redisAnswerKey)
-    if(!!cache) {
-      handleGetResponse(res, JSON.parse(cache))
-    } else {
-      let queryText = SELECT_ANSWERS
+    // let redisAnswerKey = `question_id=${question_id}&count=${count}&page=${page}`;
+    // const cache = await getCache(redisAnswerKey)
+    // if(!!cache) {
+    //   handleGetResponse(res, JSON.parse(cache))
+    // } else {
       let offset = (page - 1) * count
-      let query = {
-        text: queryText,
+      let selectAnswers = {
+        text: SELECT_ANSWERS,
         values: [question_id, count, offset]
       }
 
       pool
-        .query(query)
+        .query(selectAnswers)
         .then(async(results) => {
           let response = {
             question: question_id,
@@ -52,69 +50,54 @@ const getAnswers = async (req, res) => {
             count: count,
           }
           response.results = results.rows
-          await setCache(redisAnswerKey, JSON.stringify(response))
+        //  await setCache(redisAnswerKey, JSON.stringify(response))
           handleGetResponse(res, response)
         })
         .catch(err => {
           handleError(res, err)
         })
-    }
+   // }
   } catch(err) {
     handleError(res, err)
   }
 }
 
 
-const postAnswer = (req, res) => {
-
+const postAnswer = async(req, res) => {
   let question_id = parseInt(req.params.question_id)
-
-  let answer;
-  if(typeof req.body.data !== 'object') {
-    answer = JSON.parse(req.body.data)
-  } else {
-    answer = req.body.data
-  }
-
-  let body = answer.body
+  let { body, name, email } = req.body
+  let photos = req.body.rawPhotos
   let date_written = new Date().toISOString()
-  let name = answer.name
-  let email = answer.email
-  let photos = answer.rawPhotos
   let reported = false
   let helpful = 0
-  console.log(body, name, email, photos)
 
-  let answerQuery = {
-    text: INSERT_ANSWERS,
-    values: [question_id, body, date_written, name, email, reported, helpful]
-  }
-
-  pool
-    .query(answerQuery)
-    .then(results => {
-      let answer_id = results.rows[0].id
-      return answer_id
-    })
-    .then(answer_id => {
-      let photoQueries = []
-      for(let i = 0; i < photos.length; i++) {
-        let photoQuery = {
-          text: INSERT_PHOTOS,
-          values: [answer_id, photos[i]]
-        }
-        photoQueries.push(pool.query(photoQuery))
+  const client = await pool.connect()
+  try {
+    await client.query('BEGIN')
+    let insertAnswer = {
+      text: INSERT_ANSWER,
+      values: [question_id, body, date_written, name, email, reported, helpful]
+    }
+    let result = await client.query(insertAnswer)
+    let answer_id = result.rows[0].id
+    let photoQueries = []
+    for(let i = 0; i < photos.length; i++) {
+      let photoQuery = {
+        text: INSERT_PHOTOS,
+        values: [answer_id, photos[i]]
       }
+      photoQueries.push(client.query(photoQuery))
+    }
 
-      return Promise.all(photoQueries)
-    })
-    .then(results => {
-      console.log('AFTER PROMISE ALL: ', results)
-      handlePostResponse(res, results)
-    })
-    .catch(err => {
-      handleError(res, err)
-    })
+    await Promise.all(photoQueries)
+    await client.query('COMMIT')
+    handlePostResponse(res)
+  } catch(err) {
+    await client.query('ROLLBACK')
+    console.log(err)
+  } finally {
+    client.release()
+  }
 }
 
 
