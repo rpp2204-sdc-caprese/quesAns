d2275790910bab6cf8de5ddee2c213528168be67
Nick Hays <nicholasahays@gmail.com>
2022-11-13 21:49:17 -0700

    Uses express router routes

--- server/index.js
+++ server/index.js
@@ -2,47 +2,24 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const express = require('express')
 const morgan = require('morgan')
-const path = require('path')
-const fs = require('fs')
 const rt = require('file-stream-rotator')
 const Writable = require('stream').Writable
+const path = require('path')
+const fs = require('fs')
 const loaderio_verification_token = ''
-
-const {
-  getQuestions,
-  postQuestion,
-  updateQuestionHelpfulness,
-  reportQuestion
-} = require('./controllers/questionControllers.js')
-
-const {
-    getAnswers,
-    postAnswer,
-    updateAnswerHelpfulness,
-    reportAnswer
-} = require('./controllers/answerControllers.js')
 
 //morgan error logger
 let fileWriter = rt.getStream({filename:'errors.log', frequency:'daily', verbose: true})
 const skipSuccess = (req, res) => res.statusCode < 400
 
 const app = express()
+const router = express.Router()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(morgan('combined', { skip: skipSuccess, stream: fileWriter }))
 
-app.route('/qa/questions')
-  .get(getQuestions)
-  .post(postQuestion)
-
-app.route('/qa/questions/:question_id/answers')
-  .get(getAnswers)
-  .post(postAnswer)
-
-app.put('/qa/questions/:question_id/helpful', updateQuestionHelpfulness)
-app.put('/qa/questions/:question_id/report', reportQuestion)
-app.put('/qa/answers/:answer_id/helpful', updateAnswerHelpfulness)
-app.put('/qa/answers/:answer_id/report', reportAnswer)
+app.use('/qa/questions', require('./routes/questionRoutes'))
+app.use('/qa/answers/:answer_id', require('./routes/answerRoutes'))
 
 app.get(`/${loaderio_verification_token}.txt`, (req, res) => {
     res.send(loaderio_verification_token)
