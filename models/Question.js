824864e6ec8201d139973202f5a82e2ee06ac365
Nick Hays <nicholasahays@gmail.com>
2022-11-14 20:50:20 -0700

    Cleans up indent for Question.get

--- models/Question.js
+++ models/Question.js
@@ -6,24 +6,24 @@ const Question = {}
 Question.get = async(product_id, count, offset) => {
   const client = await pool.connect()
   let results = [];
-  return  client
-            .query(QuesQuery.select(), [product_id, count, offset])
-            .then(async(questions) => {
-                results = questions.rows
-                for(let i = 0; i < results.length; i++) {
-                  for(let answer_id in results[i].answers) {
-                    let photo_urls = await client.query(QuesQuery.selectPhotos(), [answer_id])
-                    results[i].answers[answer_id].photos = photo_urls.rows[0].photos
-                  }
-                }
-                return results;
-            })
-            .catch(err => {
-              client.query('ROLLBACK')
-              console.log('There was an issue retrieving the data')
-              return err;
-            })
-            .finally(() => client.release())
+  return client
+    .query(QuesQuery.select(), [product_id, count, offset])
+    .then(async(questions) => {
+        results = questions.rows
+        for(let i = 0; i < results.length; i++) {
+          for(let answer_id in results[i].answers) {
+            let photo_urls = await client.query(QuesQuery.selectPhotos(), [answer_id])
+            results[i].answers[answer_id].photos = photo_urls.rows[0].photos
+          }
+        }
+        return results;
+    })
+    .catch(err => {
+      client.query('ROLLBACK')
+      console.log('There was an issue retrieving the data')
+      return err;
+    })
+    .finally(() => client.release())
 }
 
 Question.create = (values) => {
