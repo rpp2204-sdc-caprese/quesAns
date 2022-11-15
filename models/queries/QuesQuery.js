1c28b23466dabffda624c4f983d351620376e90f
Nick Hays <nicholasahays@gmail.com>
2022-11-14 19:47:23 -0700

    Collections of functions that return query texts

---
+++ models/queries/QuesQuery.js
@@ -0,0 +1,57 @@
+const QuesQuery = {}
+
+QuesQuery.select = () => {
+  return `
+    SELECT
+      q.id AS question_id,
+      q.body AS question_body,
+      q.date_written AS question_date,
+      q.asker_name,
+      q.helpful AS question_helpfulness,
+      q.reported,
+      COALESCE(json_object_agg(a.id, json_build_object(
+        'id', a.id,
+        'body', a.body,
+        'date', a.date_written,
+        'answerer_name', a.answerer_name,
+        'helpfulness', a.helpful
+      ))
+      FILTER (where a.id is not null), '{}') AS answers
+    FROM
+      questions q
+    LEFT JOIN
+      answers a
+    ON
+      a.question_id = q.id
+    AND
+      a.reported = FALSE
+    WHERE
+      q.product_id = $1
+    AND
+      q.reported = FALSE
+    GROUP BY q.id
+    ORDER BY
+      q.id
+    LIMIT
+      $2
+    OFFSET
+      $3`;
+}
+
+QuesQuery.selectPhotos = () => {
+  return `SELECT COALESCE(array_agg(url), '{}') AS photos FROM answers_photos WHERE answer_id = $1`;
+}
+
+QuesQuery.insert = () => {
+  return `INSERT INTO questions(product_id, body, date_written, asker_name, asker_email, reported, helpful) values($1, $2, $3, $4, $5, $6, $7)`;
+}
+
+QuesQuery.updateHelpfulness = () => {
+  return 'UPDATE questions SET helpful = helpful + 1 WHERE id = $1';
+}
+
+QuesQuery.updateReported = () => {
+  return 'UPDATE questions SET reported = true WHERE id = $1';
+}
+
+module.exports = QuesQuery;
