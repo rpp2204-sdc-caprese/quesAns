1f6c2f219c2a9c4f8e927458ad31ce45a5f44701
Nick Hays <nicholasahays@gmail.com>
2022-11-16 00:32:19 -0700

    Moves response helpers to helpers directory

---
+++ server/controllers/helpers/resHelpers.js
@@ -0,0 +1,18 @@
+/*****RESPONSE HANDLERS*****/
+const handleGetResponse = (res, data) => res.status(200).send(data)
+const handlePostResponse = (res) => res.sendStatus(201)
+const handlePutResponse = (res) => res.sendStatus(204)
+const handleClientError = (res, err) => res.status(404).send(err)
+const handleError = (res, err) => {
+  console.log(err)
+  res.status(500).send(err)
+}
+
+module.exports = {
+  handleGetResponse,
+  handlePostResponse,
+  handlePutResponse,
+  handleClientError,
+  handleError,
+  idIsInvalid
+}
