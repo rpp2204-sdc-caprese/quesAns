28128270e3ac06833aed5ebb1ceb4e1be7597cc4
Nick Hays <nicholasahays@gmail.com>
2022-11-17 13:53:14 -0700

    Creates Redis class with collection of functions, including isRedisReady function that uses client.ping() to verify connection

--- database/redisHelpers.js
+++ database/redisHelpers.js
@@ -1,35 +1,18 @@
 const redisClient = require('./redis.js')
 const SECONDS_TIL_EXPIRATION = 3600
 
-let isInitiatingConnection = false;
-let isReady = false;
-
-redisClient
-  .on('connect', () => isInitiatingConnection = true)
-  .on('ready', () => isReady = true)
-  .on('error', () => isReady = false)
-
-const CheckRedis = {
-  isConnecting: () => {
-    return isInitiatingConnection;
+const Redis = {
+  isRedisReady: () => {
+    return redisClient.ping();
   },
-  isReady: () => {
-    return isReady;
+
+  getCache: async(key) => {
+    return await redisClient.get(key)
+  },
+
+  setCache: async(key, value) => {
+    await redisClient.set(key, SECONDS_TIL_EXPIRATION, JSON.stringify(value))
   }
 }
 
-const getCache  = async(key) => {
-  const cache = await redisClient.get(key)
-  return cache
-}
-
-const setCache = async(key, value) => {
-  await redisClient.set(key, SECONDS_TIL_EXPIRATION, JSON.stringify(value))
-}
-
-
-module.exports = {
-  getCache,
-  setCache,
-  CheckRedis
-}
+module.exports = Redis
