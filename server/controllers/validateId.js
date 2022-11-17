df20a9091c70717d797799c1ca3d0ec47266847d
Nick Hays <nicholasahays@gmail.com>
2022-11-16 21:28:53 -0700

    ValidateId middlware function

---
+++ server/controllers/validateId.js
@@ -0,0 +1,12 @@
+const { handleClientError } = require('./helpers/resHelpers.js')
+const INVALID_ID_MESSAGE = 'INVALID ID WAS SUBMITTED'
+
+const idIsInvalid = (id) => id === undefined || id < 0 || isNaN(id)
+
+const validateId = (req, res, next) => {
+  let id = req.query.product_id || req.params.question_id || req.params.answer_id
+  if(idIsInvalid(id)) return handleClientError(res, INVALID_ID_MESSAGE)
+  else next()
+}
+
+module.exports.validateId = validateId
