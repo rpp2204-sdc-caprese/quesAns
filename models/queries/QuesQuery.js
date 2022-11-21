02ca961833c2310d83d799febff64d2668d091fc
Nick Hays <nicholasahays@gmail.com>
2022-11-20 23:37:34 -0700

    Refactors to use single query

--- models/queries/QuesQuery.js
+++ models/queries/QuesQuery.js
@@ -1,41 +1,42 @@
 const QuesQuery = {}
 
 QuesQuery.select = () => {
-  return `
-    SELECT
-      q.id AS question_id,
-      q.body AS question_body,
-      q.date_written AS question_date,
-      q.asker_name,
-      q.helpful AS question_helpfulness,
-      q.reported,
-      COALESCE(json_object_agg(a.id, json_build_object(
-        'id', a.id,
-        'body', a.body,
-        'date', a.date_written,
-        'answerer_name', a.answerer_name,
-        'helpfulness', a.helpful
-      ))
-      FILTER (where a.id is not null), '{}') AS answers
-    FROM
-      questions q
-    LEFT JOIN
-      answers a
-    ON
-      a.question_id = q.id
-    AND
-      a.reported = FALSE
-    WHERE
-      q.product_id = $1
-    AND
-      q.reported = FALSE
-    GROUP BY q.id
-    ORDER BY
-      q.id
-    LIMIT
-      $2
-    OFFSET
-      $3`;
+return `
+  SELECT
+    q.id AS question_id,
+    q.body AS question_body,
+    q.date_written AS question_date,
+    q.asker_name,
+    q.helpful AS question_helpfulness,
+    q.reported,
+    COALESCE(json_object_agg(a.id, json_build_object(
+      'id', a.id,
+      'body', a.body,
+      'date', a.date_written,
+      'answerer_name', a.answerer_name,
+      'helpfulness', a.helpful
+      'photos', (SELECT COALESCE(array_agg(url), '{}') FROM answers_photos WHERE answer_id = a.id)
+    ))
+    FILTER (where a.id is not null), '{}') AS answers
+  FROM
+    questions q
+  LEFT JOIN
+    answers a
+  ON
+    a.question_id = q.id
+  AND
+    a.reported = FALSE
+  WHERE
+    q.product_id = $1
+  AND
+    q.reported = FALSE
+  GROUP BY q.id
+  ORDER BY
+    q.id
+  LIMIT
+    $2
+  OFFSET
+    $3`;
 }
 
 QuesQuery.selectPhotos = () => {
