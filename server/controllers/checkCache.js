bf50517688de79c1637deaa75ebcb406a168d89b
Nick Hays <nicholasahays@gmail.com>
2022-11-21 00:45:10 -0700

    Removes keyword async and gets rid of check redis connection check

--- server/controllers/checkCache.js
+++ server/controllers/checkCache.js
@@ -1,39 +1,17 @@
 const { getCache, isRedisReady} = require('../../database/redisHelpers.js')
 const { handleGetResponse, handleClientError } = require('./helpers/resHelpers.js')
-//let redisIsReady = isRedisReady()
 
-const checkCache = async(req, res, next) => {
-  // try {
+const checkCache = (req, res, next) => {
     const { product_id } = req.query
     const count = req.query.count || 5
     const page = req.query.page || 1
     let redisQuestionKey = req.redisQuestionKey = `product_id=${product_id}&count=${count}&page=${page}`
-    /**PROMISES */
     getCache(redisQuestionKey)
       .then((cache) => {
         if(cache) handleGetResponse(res, JSON.parse(cache))
         else next()
       })
       .catch((err) => handleClientError(res, err))
-
-
-
-
-
-
-  //   if(true) {
-  //     const cache = await getCache(redisQuestionKey)
-  //     if(!!cache) {
-  //       handleGetResponse(res, JSON.parse(cache))
-  //     } else /*RESULT IS NOT CACHED*/ {
-  //       next()
-  //     }
-  //   } else /*REDIS IS NOT CONNECTED*/ {
-  //     next()
-  //   }
-  // } catch(err) {
-  //   handleClientError(res,err)
-  // }
 }
 
 module.exports.checkCache = checkCache
