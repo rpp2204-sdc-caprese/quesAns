efcc8ee6dee617fae6566087164916372eca92a6
Nick Hays <nicholasahays@gmail.com>
2022-11-21 00:51:31 -0700

    Streamlines functions and removes async await and removes then and catch block from getCache

--- database/redisHelpers.js
+++ database/redisHelpers.js
@@ -2,20 +2,9 @@ const redisClient = require('./redis.js')
 const SECONDS_TIL_EXPIRATION = 3600
 
 const Redis = {
-  isRedisReady: async() => {
-    return redisClient.ping();
-  },
-
-  getCache: async(key) => {
-    return redisClient
-      .get(key)
-      .then(cache => cache)
-      .catch(err => err)
-  },
-
-  setCache: async(key, value) => {
-    await redisClient.setEx(key, SECONDS_TIL_EXPIRATION, JSON.stringify(value))
-  }
+  isRedisReady: () => redisClient.ping(),
+  getCache: (key) => redisClient.get(key),
+  setCache: (key, value) => redisClient.setEx(key, SECONDS_TIL_EXPIRATION, JSON.stringify(value))
 }
 
 module.exports = Redis
