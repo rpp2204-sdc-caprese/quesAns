d9484168fd0c343231e044bb6d5a266d91417b12
Nick Hays <nicholasahays@gmail.com>
2022-11-20 22:15:50 -0700

    Creates custom db methods so as to not expose pg pool

--- database/db.js
+++ database/db.js
@@ -19,6 +19,10 @@ try {
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
-  queryAwait: async(text, params) => await pool.query(text, params),
-  connect: async() => await pool.connect()
+  clientQuery: (client, text, params) => client.query(text, params),
+  clientBegin: (client, text) => client.query('BEGIN'),
+  clientCommit: (client, text) => client.query('COMMIT'),
+  clientRollback: (client, text) => client.query('ROLLBACK'),
+  clientRelease: (client) => client.release(),
+  connect: () => pool.connect()
 }
